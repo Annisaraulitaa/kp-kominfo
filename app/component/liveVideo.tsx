564476192fb040854e548d773903120b3b18805a
Annisaraulitaa <annisaraulita240702@gmail.com>
2025-01-09 11:55:01 +0800

    update live video

--- app/component/liveVideo.tsx
+++ app/component/liveVideo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Live(){
     return(
-        <div className='flex justify-center items-center h-screen bg-gray-100'>
+        <div className='flex justify-center items-center bg-gray-100'>
             <div className='w-full max-w-4xl border border-gray-300 rounded-lg shadow-lg overflow-hidden'>
                 <div className='bg-gray-800 text-white text-center py-2 text-lg font-semibold'>
                 Live
