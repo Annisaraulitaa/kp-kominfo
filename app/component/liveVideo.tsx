9eca67b4fca6ab50ac317629de8592d368f9d3f9
Annisaraulitaa <annisaraulita240702@gmail.com>
2025-01-09 10:49:26 +0800

    add fe live video cctv

---
+++ app/component/liveVideo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Live(){
+    return(
+        <div className='flex justify-center items-center h-screen bg-gray-100'>
+            <div className='w-full max-w-4xl border border-gray-300 rounded-lg shadow-lg overflow-hidden'>
+                <div className='bg-gray-800 text-white text-center py-2 text-lg font-semibold'>
+                Live
+                </div>
+                <div className='aspect-w-16 aspect-h-9 bg-black'>
+                    <video
+                    autoPlay
+                    loop
+                    controls
+                    className='w-full h-full'
+                    src='/* url live feed */'/>
+                </div>
+            </div>
+        </div>
+    )
+}
